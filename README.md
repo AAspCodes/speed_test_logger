e15d21d1430a6526e4b98736cd3b769f189cecc4
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2023-09-08 19:14:24 -0700

    fix: add run_cmd

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # speed_test_logger
 rust internet speed test logger
+
+
+
+
+## Goals
+
+- run internet speed test with
+    - latency
+    - download speed
+    - upload speed
+- write results to sqlite
+- can either produce graphs or send resuls in json format
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let result = run_cmd(String::from("ls"));
+    println!("{}", result)
+}
+
+fn run_cmd(cmd: String) -> String {
+    let output = Command::new("sh")
+        .args(["-c", cmd.as_str()])
+        .output()
+        .expect("failed to execute process");
+
+    let result = String::from_utf8(output.stdout).unwrap();
+    return result;
 }
